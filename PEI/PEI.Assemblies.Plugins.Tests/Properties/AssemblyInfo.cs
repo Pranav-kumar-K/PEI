726ef559d735b72268491495687b1b269e01493f
pranav kumar <pranavKofficial@gmail.com>
2026-04-05 23:44:48 +0530

    Add unit test project for plugins and JS validation notes
    Added PEI.Assemblies.Plugins.Tests with MSTest and FakeXrmEasy for plugin unit testing, covering account and contact scenarios. Updated solution to include the test project. Added documentation on client-side JS validation concerns.

---
+++ PEI/PEI.Assemblies.Plugins.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PEI.Assemblies.Plugins.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PEI.Assemblies.Plugins.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0dce08f0-1fdd-4e97-bf3f-f1c3e04ad8cb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
