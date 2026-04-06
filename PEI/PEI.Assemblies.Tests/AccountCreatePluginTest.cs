e9bdf7bc41c17125b3dd2e12b3aaf20f933c2353
pranav kumar <pranavKofficial@gmail.com>
2026-04-07 00:00:46 +0530

    Unit Tests

---
+++ PEI/PEI.Assemblies.Tests/AccountCreatePluginTest.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FakeXrmEasy;
+using Microsoft.Xrm.Sdk;
+using System.Linq;
+using PEI.Assemblies.Plugins;
+
+namespace PEI.Assemblies.Plugins.Tests
+{
+    [TestClass]
+    public class AccountCreatePluginTest
+    {
+        [TestMethod]
+        public void Should_Create_Contact_On_Account_Create()
+        {
+            // Arrange
+            var context = new XrmFakedContext();
+            var service = context.GetOrganizationService();
+
+            var account = new Entity("account")
+            {
+                Id = System.Guid.NewGuid()
+            };
+            account["name"] = "Test Account";
+
+            var pluginContext = context.GetDefaultPluginContext();
+            pluginContext.InputParameters["Target"] = account;
+            pluginContext.MessageName = "Create";
+
+            // Act
+            context.ExecutePluginWith<AccountCreatePlugin>(pluginContext);
+
+            // Assert
+            var contacts = context.CreateQuery("contact").ToList();
+
+            Assert.AreEqual(1, contacts.Count);
+            Assert.AreEqual("Default", contacts[0]["firstname"]);
+            Assert.AreEqual("Test Account", contacts[0]["lastname"]);
+        }
+
+        [TestMethod]
+        public void Should_Not_Create_Contact_If_Not_Account()
+        {
+            // Arrange
+            var context = new XrmFakedContext();
+
+            var lead = new Entity("lead")
+            {
+                Id = System.Guid.NewGuid()
+            };
+
+            var pluginContext = context.GetDefaultPluginContext();
+            pluginContext.InputParameters["Target"] = lead;
+            pluginContext.MessageName = "Create";
+
+            // Act
+            context.ExecutePluginWith<AccountCreatePlugin>(pluginContext);
+
+            // Assert
+            var contacts = context.CreateQuery("contact").ToList();
+            Assert.AreEqual(0, contacts.Count);
+        }
+
+        [TestMethod]
+        public void Should_Handle_Account_With_No_Name()
+        {
+            // Arrange
+            var context = new XrmFakedContext();
+
+            var account = new Entity("account")
+            {
+                Id = System.Guid.NewGuid()
+            };
+
+            var pluginContext = context.GetDefaultPluginContext();
+            pluginContext.InputParameters["Target"] = account;
+            pluginContext.MessageName = "Create";
+
+            // Act
+            context.ExecutePluginWith<AccountCreatePlugin>(pluginContext);
+
+            // Assert
+            var contacts = context.CreateQuery("contact").ToList();
+
+            Assert.AreEqual(1, contacts.Count);
+            Assert.AreEqual("Default", contacts[0]["firstname"]);
+            Assert.IsTrue(contacts[0].Contains("lastname") == false || contacts[0]["lastname"] == null);
+        }
+    }
+}
---
+++ PEI/PEI.Assemblies.Tests/DuplicateContactValidationPluginTest.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FakeXrmEasy;
+using Microsoft.Xrm.Sdk;
+using System;
+using PEI.Assemblies.Plugins;
+
+namespace PEI.Assemblies.Plugins.Tests
+{
+    [TestClass]
+    public class DuplicateContactValidationPluginTest
+    {
+        [TestMethod]
+        public void Should_Throw_Error_When_Duplicate_Email_Exists()
+        {
+            // Arrange
+            var context = new XrmFakedContext();
+
+            var existingContact = new Entity("contact")
+            {
+                Id = Guid.NewGuid()
+            };
+            existingContact["emailaddress1"] = "test@mail.com";
+
+            context.Initialize(new[] { existingContact });
+
+            var newContact = new Entity("contact");
+            newContact["emailaddress1"] = "test@mail.com";
+
+            var pluginContext = context.GetDefaultPluginContext();
+            pluginContext.InputParameters["Target"] = newContact;
+            pluginContext.MessageName = "Create";
+
+            // Act & Assert
+            Assert.ThrowsException<InvalidPluginExecutionException>(() =>
+                context.ExecutePluginWith<DuplicateContactValidationPlugin>(pluginContext)
+            );
+        }
+
+        [TestMethod]
+        public void Should_Allow_When_Email_Is_Unique()
+        {
+            // Arrange
+            var context = new XrmFakedContext();
+
+            var contact = new Entity("contact");
+            contact["emailaddress1"] = "unique@mail.com";
+
+            var pluginContext = context.GetDefaultPluginContext();
+            pluginContext.InputParameters["Target"] = contact;
+            pluginContext.MessageName = "Create";
+
+            // Act
+            context.ExecutePluginWith<DuplicateContactValidationPlugin>(pluginContext);
+
+            // Assert
+            Assert.IsTrue(true); // No exception = success
+        }
+
+        [TestMethod]
+        public void Should_Skip_When_Email_Is_Empty()
+        {
+            // Arrange
+            var context = new XrmFakedContext();
+
+            var contact = new Entity("contact");
+
+            var pluginContext = context.GetDefaultPluginContext();
+            pluginContext.InputParameters["Target"] = contact;
+            pluginContext.MessageName = "Create";
+
+            // Act
+            context.ExecutePluginWith<DuplicateContactValidationPlugin>(pluginContext);
+
+            // Assert
+            Assert.IsTrue(true); // Plugin should not fail
+        }
+    }
+}
